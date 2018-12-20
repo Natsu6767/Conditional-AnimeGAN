91550aa6e6ba7bde05fdf52582fffc12a81c3a30
Mohit Jain <mohitjain1999@yahoo.com>
2018-12-21 01:16:40 +0400

    Add threshold for word count to add to vocabulary.

--- clean_data.py
+++ clean_data.py
@@ -6,6 +6,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-load_path', default='data/tags_clean.csv', help='Path to csv data file.')
 parser.add_argument('-save_path', default='data/clean_labels.csv', help="Path to save cleaned data.")
 parser.add_argument('-json_save_path', default='data/animegan_params.json', help="Path to save json.")
+parser.add_argument('-word_count_threshold', default=25, help="Word count threshold.")
 
 args = parser.parse_args()
 
@@ -32,6 +33,9 @@ with open(args.save_path, 'w') as g:
                     if (s_tag[0] == 'long' or s_tag[0] == 'short'):
                         continue
                     if(w == 'eyes' or w == 'hair'):
+                        if(attrib[w] != '<UNK>'):
+                            continue
+
                         attrib[w] = s_tag[0]
                         word_counts[s_tag[0]] = word_counts.get(s_tag[0], 0) + 1
             
@@ -39,7 +43,8 @@ with open(args.save_path, 'w') as g:
 
 # Store the mapping for the words as a json
 word_counts_sorted = sorted(word_counts.items(), key=lambda kv: kv[1], reverse=True)
-vocab = [color[0] for color in word_counts_sorted]
+
+vocab = [color[0] for color in word_counts_sorted if color[1] >= args.word_count_threshold]
 
 word2ind = {color: color_ind + 1 for color_ind, color in enumerate(vocab)}
 

5062ef96417c805c88c8079244d8124c3781b93c
Mohit Jain <mohitjain1999@yahoo.com>
2018-12-21 19:11:13 +0400

    Create and save ind2color dictionary.

--- clean_data.py
+++ clean_data.py
@@ -6,11 +6,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-load_path', default='data/tags_clean.csv', help='Path to csv data file.')
 parser.add_argument('-save_path', default='data/clean_labels.csv', help="Path to save cleaned data.")
 parser.add_argument('-json_save_path', default='data/animegan_params.json', help="Path to save json.")
-parser.add_argument('-word_count_threshold', default=25, help="Word count threshold.")
+parser.add_argument('-color_count_threshold', default=25, help="Word count threshold.")
 
 args = parser.parse_args()
 
-word_counts = {}
+color_counts = {}
 
 with open(args.save_path, 'w') as g:
     writer = csv.writer(g)
@@ -37,17 +37,21 @@ with open(args.save_path, 'w') as g:
                             continue
 
                         attrib[w] = s_tag[0]
-                        word_counts[s_tag[0]] = word_counts.get(s_tag[0], 0) + 1
+                        color_counts[s_tag[0]] = color_counts.get(s_tag[0], 0) + 1
             
             writer.writerow([ridx, attrib['eyes'], attrib['hair']])
 
 # Store the mapping for the words as a json
-word_counts_sorted = sorted(word_counts.items(), key=lambda kv: kv[1], reverse=True)
+color_counts_sorted = sorted(color_counts.items(), key=lambda kv: kv[1], reverse=True)
 
-vocab = [color[0] for color in word_counts_sorted if color[1] >= args.word_count_threshold]
+vocab = [color[0] for color in color_counts_sorted if color[1] >= args.color_count_threshold]
 
-word2ind = {color: color_ind + 1 for color_ind, color in enumerate(vocab)}
+color2ind = {color: color_ind + 1 for color_ind, color in enumerate(vocab)}
+ind2color = {color_ind: color for color, color_ind in color2ind.items()}
+
+out = {}
+out['ind2color'] = ind2color
+out['color2ind'] = color2ind
 
 with open(args.json_save_path, 'w') as fp:
-    json.dump(word2ind, fp)
-
+    json.dump(out, fp)

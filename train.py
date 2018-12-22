c1fe289ced2997bad736b37a56fd9918d4c6e05b
Mohit Jain <mohitjain1999@yahoo.com>
2018-12-22 21:11:45 +0400

    Create train script to train the model.

---
+++ train.py
@@ -0,0 +1,249 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import random
+import json
+
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from dataloader import AnimeDataset
+from model import weights_init, Generator, Discriminator
+
+# Set random seed for reproducibility.
+seed = 369
+random.seed(seed)
+torch.manual_seed(seed)
+print("Random Seed: ", seed)
+
+# Parameters to define the model.
+params = {
+    "bsize" : 128,# Batch size during training.
+    'imsize' : 64,# Spatial size of training images. All images will be resized to this size during preprocessing.
+    'nc' : 3,# Number of channles in the training images. For coloured images this is 3.
+    'nz' : 100,# Size of the Z latent vector (the input to the generator).
+    'ngf' : 64,# Size of feature maps in the generator. The depth will be multiples of this.
+    'ndf' : 64, # Size of features maps in the discriminator. The depth will be multiples of this.
+    'nepochs' : 10,# Number of training epochs.
+    'lr' : 0.0002,# Learning rate for optimizers
+    'beta1' : 0.5,# Beta1 hyperparam for Adam optimizer
+    'save_epoch' : 2}# Save step.
+
+# Use GPU is available else use CPU.
+device = torch.device("cuda:0" if(torch.cuda.is_available()) else "cpu")
+print(device, " will be used.\n")
+
+# Get the data.
+transform = transforms.Compose([
+    transforms.Resize(params['imsize']),
+    transforms.CenterCrop(params['imsize']),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5),
+        (0.5, 0.5, 0.5))])
+
+anime_dataset = AnimeDataset(csv_file='data/clean_labels.csv',
+                             root_dir='data/faces/',
+                             param_file='data/animegan_params.json',
+                             transform=transform)
+
+dataloader = DataLoader(anime_dataset, batch_size=params['bsize'],
+                        shuffle=True)
+
+# Plot the training images.
+sample_batch = next(iter(dataloader))
+plt.figure(figsize=(8, 8))
+plt.axis("off")
+plt.title("Training Images")
+plt.imshow(np.transpose(vutils.make_grid(
+    sample_batch['image'].to(device)[ : 64], padding=2, normalize=True).cpu(), (1, 2, 0)))
+
+plt.show()
+
+params['n_conditions'] = len(sample_batch['colors'][0])
+
+with open('data/animegan_params.json', 'r') as info_file:
+            info = json.load(info_file)
+            ind2color = info['ind2color']
+            color2ind = info['color2ind']
+
+params['vocab_size'] = len(ind2color) + 1
+params['embedding_size'] = params['vocab_size']
+
+# Create the generator.
+netG = Generator(params).to(device)
+# Apply the weights_init() function to randomly initialize all
+# weights to mean=0.0, stddev=0.2
+netG.apply(weights_init)
+# Print the model.
+print(netG)
+
+# Create the discriminator.
+netD = Discriminator(params).to(device)
+# Apply the weights_init() function to randomly initialize all
+# weights to mean=0.0, stddev=0.2
+netD.apply(weights_init)
+# Print the model.
+print(netD)
+
+# Binary Cross Entropy loss function.
+criterion = nn.BCELoss()
+
+onehot = torch.zeros(params['vocab_size'], params['vocab_size'])
+onehot = onehot.scatter_(1, torch.LongTensor([i for i in range(params['vocab_size'])]).view(params['vocab_size'], 1), 1).view(params['vocab_size'], params['vocab_size'], 1, 1)
+
+
+fixed_noise = torch.randn(64, params['nz'], 1, 1, device=device)
+#Contructing fixed conditions
+frow1 = torch.cat((torch.ones(8, 1)*color2ind['blue'], torch.ones(8, 1)*color2ind['blonde']), dim=1)
+frow2 = torch.cat((torch.ones(8, 1)*color2ind['brown'], torch.ones(8, 1)*color2ind['blue']), dim=1)
+frow3 = torch.cat((torch.ones(8, 1)*color2ind['red'], torch.ones(8, 1)*color2ind['black']), dim=1)
+frow4 = torch.cat((torch.ones(8, 1)*color2ind['purple'], torch.ones(8, 1)*color2ind['white']), dim=1)
+frow5 = torch.cat((torch.ones(8, 1)*color2ind['green'], torch.ones(8, 1)*color2ind['black']), dim=1)
+frow6 = torch.cat((torch.ones(8, 1)*color2ind['bicolored'], torch.ones(8, 1)*color2ind['yellow']), dim=1)
+frow7 = torch.cat((torch.ones(8, 1)*color2ind['gray'], torch.ones(8, 1)*color2ind['pink']), dim=1)
+frow8 = torch.cat((torch.ones(8, 1)*color2ind['blue'], torch.ones(8, 1)*color2ind['red']), dim=1)
+fixed_condition = torch.cat((frow1, frow2, frow3, frow4, frow5,
+                                frow6, frow7, frow8), dim=0).type(torch.LongTensor)
+
+fixed_condition_ohe = torch.cat((onehot[fixed_condition[:, 0]], onehot[fixed_condition[:, 1]]), dim=1).to(device)
+
+real_label = 1
+fake_label = 0
+
+# Optimizer for the discriminator.
+optimizerD = optim.Adam(netD.parameters(), lr=params['lr'], betas=(params['beta1'], 0.999))
+# Optimizer for the generator.
+optimizerG = optim.Adam(netG.parameters(), lr=params['lr'], betas=(params['beta1'], 0.999))
+
+# Stores generated images as training progresses.
+img_list = []
+# Stores generator losses during training.
+G_losses = []
+# Stores discriminator losses during training.
+D_losses = []
+
+iters = 0
+
+print("Starting Training Loop...")
+print("-"*25)
+
+for epoch in range(params['nepochs']):
+    for i, data in enumerate(dataloader, 0):
+        # Transfer data tensor to GPU/CPU (device)
+        real_images = data['image'].to(device)
+        y = data['colors']
+        condition = torch.cat((onehot[y[:, 0]], onehot[y[:, 1]]), dim=1).to(device)
+        # Get batch size. Can be different from params['nbsize'] for last batch in epoch.
+        b_size = real_images.size(0)
+        
+        # Make accumalated gradients of the discriminator zero.
+        netD.zero_grad()
+        # Create labels for the real data. (label=1)
+        label = torch.full((b_size, ), real_label, device=device)
+        output = netD(real_images, condition, params).view(-1)
+        errD_real = criterion(output, label)
+        # Calculate gradients for backpropagation.
+        errD_real.backward()
+        D_x = output.mean().item()
+        
+        # Sample random data from a unit normal distribution.
+        noise = torch.randn(b_size, params['nz'], 1, 1, device=device)
+        # Generate fake data (images).
+        fake_data = netG(noise, condition)
+        # Create labels for fake data. (label=0)
+        label.fill_(fake_label)
+        # Calculate the output of the discriminator of the fake data.
+        # As no gradients w.r.t. the generator parameters are to be
+        # calculated, detach() is used. Hence, only gradients w.r.t. the
+        # discriminator parameters will be calculated.
+        # This is done because the loss functions for the discriminator
+        # and the generator are slightly different.
+        output = netD(fake_data.detach(), condition, params).view(-1)
+        errD_fake = criterion(output, label)
+        # Calculate gradients for backpropagation.
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+
+        # Net discriminator loss.
+        errD = errD_real + errD_fake
+        # Update discriminator parameters.
+        optimizerD.step()
+        
+        # Make accumalted gradients of the generator zero.
+        netG.zero_grad()
+        # We want the fake data to be classified as real. Hence
+        # real_label are used. (label=1)
+        label.fill_(real_label)
+        # No detach() is used here as we want to calculate the gradients w.r.t.
+        # the generator this time.
+        output = netD(fake_data, condition, params).view(-1)
+        errG = criterion(output, label)
+        # Gradients for backpropagation are calculated.
+        # Gradients w.r.t. both the generator and the discriminator
+        # parameters are calculated, however, the generator's optimizer
+        # will only update the parameters of the generator. The discriminator
+        # gradients will be set to zero in the next iteration by netD.zero_grad()
+        errG.backward()
+
+        D_G_z2 = output.mean().item()
+        # Update generator parameters.
+        optimizerG.step()
+
+        # Check progress of training.
+        if i%50 == 0:
+            print(torch.cuda.is_available())
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                  % (epoch, params['nepochs'], i, len(dataloader),
+                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+
+        # Save the losses for plotting.
+        G_losses.append(errG.item())
+        D_losses.append(errD.item())
+
+        # Check how the generator is doing by saving G's output on a fixed noise.
+        if (iters % 100 == 0) or ((epoch == params['nepochs']-1) and (i == len(dataloader)-1)):
+            with torch.no_grad():
+                fake_data = netG(fixed_noise, fixed_condition_ohe).detach().cpu()
+            img_list.append(vutils.make_grid(fake_data, padding=2, normalize=True))
+
+        iters += 1
+
+    # Save the model.
+    if epoch % params['save_epoch'] == 0:
+        torch.save({
+            'generator' : netG.state_dict(),
+            'discriminator' : netD.state_dict(),
+            'optimizerG' : optimizerG.state_dict(),
+            'optimizerD' : optimizerD.state_dict(),
+            'params' : params
+            }, 'checkpoint/model_epoch_{}.pth'.format(epoch))
+
+# Save the final trained model.
+torch.save({
+            'generator' : netG.state_dict(),
+            'discriminator' : netD.state_dict(),
+            'optimizerG' : optimizerG.state_dict(),
+            'optimizerD' : optimizerD.state_dict(),
+            'params' : params
+            }, 'checkpoint/model_final.pth')
+
+# Plot the training losses.
+plt.figure(figsize=(10,5))
+plt.title("Generator and Discriminator Loss During Training")
+plt.plot(G_losses,label="G")
+plt.plot(D_losses,label="D")
+plt.xlabel("iterations")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
+# Animation showing the improvements of the generator.
+fig = plt.figure(figsize=(8,8))
+plt.axis("off")
+ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+anim = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+plt.show()
+anim.save('anime.gif', dpi=80, writer='imagemagick')
